ca6884fad246ed124bf1fee15c241b08ab981f93
cos <3450414733@qq.com>
2021-12-17 18:32:49 +0800

    del:恢复出厂设置

--- utils/api.js
+++
@@ -1,9 +0,0 @@
-import http from './http.js' //引入
-const app = getApp();
-/***********个人代办相关************/
-// 获取个人自己的待办
-// const getAllTodo = () => {
-//   return http({
-//     url:'/api/news/list-all'
-//   })
-// }
